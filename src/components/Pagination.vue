cb73e22166c7cbcc2fe721cb3f6cf32026f24fd1
Mehedi Hasan Tushar <16156612+hasantushar@users.noreply.github.com>
2022-08-21 02:36:26 +0600

    Pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,9 +1,11 @@
 <template>
-Hello
 
+<div class="pagination">
 <div v-for="index in count" :key="index">
     <button @click="currentPage(index)" >{{ index }}</button>
 </div>
+</div>
+
 </template>
 
 <script setup>
@@ -11,8 +13,18 @@ import { ref, toRef, defineProps } from 'vue';
 
 defineProps({count: Number})
 
+const emit = defineEmits(['current-page'])
 
 const currentPage = (index) => {
     console.log("page", index);
+    emit('current-page', index);
 }
 </script>
+
+<style scoped>
+
+.pagination {
+    display: flex;
+}
+
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,36 +1,52 @@
 <template>
-{{currentPage}}
+<Pagination :count="todos.length/10" @current-page="setCurrentPage" />
+
 <input v-model="searchText" placeholder="Search" />
 
 <button @click="searchTodos">Search</button>
 <ul>
-    <li v-for="todo in todos" :key="todo.id">
+    <li v-for="todo in todoPaginated" :key="todo.id">
     {{ todo.title }}
     </li>
 </ul>
 
 <h3>{{ searchResult }}</h3>
 
-<Pagination :count="todos.length" />
-
 </template>
 
 <script setup>
+import { computed } from "@vue/reactivity";
 import { ref, onMounted, toRef } from "vue";
 import Pagination from "./Pagination.vue";
 
 const todos = ref([]);
 const searchText = ref("");
-const searchResult = ref("")
+const searchResult = ref("");
+const currentPage = ref("");
+
+const todoPaginated = computed(() => {
+    if(currentPage.value) {
+        return todos.value.slice((currentPage.value -1) * 10 , currentPage.value * 10);
+    }
+
+    return todos.value.slice(0 , 10);
+})
 
 const searchTodos = () => {
     searchResult.value = todos.value.filter(t => t.title === searchText.value)[0]
 }
 
+const setCurrentPage = (index) => {
+    console.log('SetCurrentPage', index);
+    currentPage.value = index;
+}
+
 onMounted(async () => {
     let res = await fetch("https://jsonplaceholder.typicode.com/todos")
     todos.value = await res.json();
-    todos.value = todos.value.slice(0, 10);
+    // todos.value = todos.value.slice(0, 10);
+    // fetch("https://jsonplaceholder.typicode.com/todos").then(async (r) => await r.json()).then(r=>console.log(r))
+
 })
 
 
