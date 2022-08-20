b3cb5d47818ccd14667bc55739868ca664275e67
Mehedi Hasan Tushar <16156612+hasantushar@users.noreply.github.com>
2022-08-21 03:15:24 +0600

    Search While Typing

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,30 +1,32 @@
 <template>
 
-<div class="pagination">
-<div v-for="index in count" :key="index">
-    <button @click="currentPage(index)" >{{ index }}</button>
-</div>
-</div>
+  <div class="pagination">
 
+    <div v-for="index in count" :key="index">
+
+      <button @click="currentPage(index)">{{ index }}</button>
+
+    </div>
+    
+  </div>
 </template>
 
 <script setup>
-import { ref, toRef, defineProps } from 'vue';
+import { ref, toRef, defineProps } from "vue";
 
-defineProps({count: Number})
+defineProps({ count: Number });
 
-const emit = defineEmits(['current-page'])
+const emit = defineEmits(["current-page"]);
 
 const currentPage = (index) => {
-    console.log("page", index);
-    emit('current-page', index);
-}
+  console.log("page", index);
+  emit("current-page", index);
+};
+
 </script>
 
 <style scoped>
-
 .pagination {
-    display: flex;
+  display: flex;
 }
-
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,22 +1,29 @@
 <template>
-<Pagination :count="todos.length/10" @current-page="setCurrentPage" />
+  <Pagination :count="todos.length / 10" @current-page="setCurrentPage" />
 
-<input v-model="searchText" placeholder="Search" />
+  <input v-model="searchText" placeholder="Search" />
 
-<button @click="searchTodos">Search</button>
-<ul>
+  <button @click="searchTodos">Search</button>
+
+  <ul v-if="!searchText">
+
     <li v-for="todo in todoPaginated" :key="todo.id">
-    {{ todo.title }}
+      {{ todo.title }}
     </li>
-</ul>
 
-<h3>{{ searchResult }}</h3>
+  </ul>
 
+  <div v-else>
+
+    <h2>Search Result</h2>
+    <h4 v-for="todo in searchResult" :key="todo.id">{{ todo.title }}</h4>
+    
+  </div>
 </template>
 
 <script setup>
 import { computed } from "@vue/reactivity";
-import { ref, onMounted, toRef } from "vue";
+import { ref, onMounted, watch } from "vue";
 import Pagination from "./Pagination.vue";
 
 const todos = ref([]);
@@ -25,29 +32,39 @@ const searchResult = ref("");
 const currentPage = ref("");
 
 const todoPaginated = computed(() => {
-    if(currentPage.value) {
-        return todos.value.slice((currentPage.value -1) * 10 , currentPage.value * 10);
-    }
+  if (currentPage.value) {
+    return todos.value.slice(
+      (currentPage.value - 1) * 10,
+      currentPage.value * 10
+    );
+  }
 
-    return todos.value.slice(0 , 10);
-})
+  return todos.value.slice(0, 10);
+});
 
 const searchTodos = () => {
-    searchResult.value = todos.value.filter(t => t.title === searchText.value)[0]
-}
+  searchResult.value = todos.value.filter((t) =>
+    t.title.includes(searchText.value)
+  );
+};
 
 const setCurrentPage = (index) => {
-    console.log('SetCurrentPage', index);
-    currentPage.value = index;
-}
+  console.log("SetCurrentPage", index);
+  currentPage.value = index;
+};
 
 onMounted(async () => {
-    let res = await fetch("https://jsonplaceholder.typicode.com/todos")
-    todos.value = await res.json();
-    // todos.value = todos.value.slice(0, 10);
-    // fetch("https://jsonplaceholder.typicode.com/todos").then(async (r) => await r.json()).then(r=>console.log(r))
+  let res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  todos.value = await res.json();
+  // todos.value = todos.value.slice(0, 10);
+  // fetch("https://jsonplaceholder.typicode.com/todos").then(async (r) => await r.json()).then(r=>console.log(r))
+});
 
-})
-
-
+watch(searchText, (newText, oldText) => {
+  if (!newText) {
+    searchResult.value = null;
+  } else {
+    searchTodos();
+  }
+});
 </script>
